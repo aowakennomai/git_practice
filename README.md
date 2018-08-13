c96359de0de611e6b6f0bfed2d4dd534052f46a2
peayton.chen <peayton.chen@104.com.tw>
2018-08-13 14:07:50 +0800

    case03 init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # git_practice
 test some git situation
+
+case03. 在 method 中間新增東西，但順序在 dev 跟 master 時不一樣
---
+++ case03/method.js
@@ -0,0 +1,23 @@
+function add(x, y) {
+	return x + y;
+}
+
+function sub(x, y) {
+	return x - y;
+}
+
+/**
+ * 回傳 x 乘 y
+ * @param x
+ * @param y
+ * @returns
+ */
+function multipy(x, y) {
+	// 乘法後回傳
+	return x * y;
+}
+
+function devide(x, y) {
+	// 簡單的 x 除以 y
+	return x / y;
+}
